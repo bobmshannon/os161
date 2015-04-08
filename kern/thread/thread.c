39ed7ba858be3134fe79d9e2e7463399299e931c
root <rshannon@buffalo.edu>
2015-04-08 19:20:43 -0400

    Kernel programs running. km1 & km2 passing.

--- kern/thread/thread.c
+++ kern/thread/thread.c
@@ -436,7 +436,7 @@ thread_bootstrap(void)
 
 	/* Initialize the process table. */
 	init_process_table();
-
+	
 	/*
 	 * Create the cpu structure for the bootup CPU, the one we're
 	 * currently running on. Assume the hardware number is 0; that
@@ -462,7 +462,7 @@ thread_bootstrap(void)
 	 */
 	curthread->t_cpu = curcpu;
 	curcpu->c_curthread = curthread;
-
+	
 	/* Done */
 }
 
@@ -625,7 +625,7 @@ thread_fork(const char *name,
 	/* Thread subsystem fields */
 	newthread->t_cpu = curthread->t_cpu;
 
-	/* Copy file table from parent to child. */ 
+	/* Copy file table from parent to child.
 	int i;
 	for(i = 0; i < OPEN_MAX; i++) {
 		newthread->t_fd_table[i] = kmalloc(sizeof(struct fd));
@@ -640,13 +640,13 @@ thread_fork(const char *name,
 		newthread->t_fd_table[i]->vn = curthread->t_fd_table[i]->vn;
 		newthread->t_fd_table[i]->lock = curthread->t_fd_table[i]->lock;
 		//newthread->t_fd_table[i]->lock = lock_create("lock");
-	}
+	} */ 
 
 	/* Copy parents address space */
 	struct addrspace **retaddr;
 	int err;
 	retaddr = kmalloc(sizeof(struct addrspace));
-	if(curthread->t_addrspace == NULL) {
+	if(retaddr == NULL) {
 		panic("parent address space null.");
 	}
 	err = as_copy(curthread->t_addrspace, retaddr);
