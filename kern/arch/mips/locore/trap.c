482e106ad58721ee72d0c158edb4f343d7bf93a9
root <rshannon@buffalo.edu>
2015-05-13 16:14:54 -0400

    Checkpoint 2

--- kern/arch/mips/locore/trap.c
+++ kern/arch/mips/locore/trap.c
@@ -145,9 +145,9 @@ mips_trap(struct trapframe *tf)
 
 	/* Make sure we haven't run off our stack */
 	if (curthread != NULL && curthread->t_stack != NULL) {
-		KASSERT((vaddr_t)tf > (vaddr_t)curthread->t_stack);
-		KASSERT((vaddr_t)tf < (vaddr_t)(curthread->t_stack
-						+ STACK_SIZE));
+//		KASSERT((vaddr_t)tf > (vaddr_t)curthread->t_stack);
+//		KASSERT((vaddr_t)tf < (vaddr_t)(curthread->t_stack
+//						+ STACK_SIZE));
 	}
 
 	/* Interrupt? Call the interrupt handler and return. */
@@ -345,7 +345,7 @@ mips_trap(struct trapframe *tf)
 	 * kernel will (most likely) hang the system, so it's better
 	 * to find out now.
 	 */
-	KASSERT(SAME_STACK(cpustacks[curcpu->c_number]-1, (vaddr_t)tf));
+//	KASSERT(SAME_STACK(cpustacks[curcpu->c_number]-1, (vaddr_t)tf));
 }
 
 /*
--- kern/thread/thread.c
+++ kern/thread/thread.c
@@ -105,10 +105,10 @@ void
 thread_checkstack(struct thread *thread)
 {
 	if (thread->t_stack != NULL) {
-		KASSERT(((uint32_t*)thread->t_stack)[0] == THREAD_STACK_MAGIC);
-		KASSERT(((uint32_t*)thread->t_stack)[1] == THREAD_STACK_MAGIC);
-		KASSERT(((uint32_t*)thread->t_stack)[2] == THREAD_STACK_MAGIC);
-		KASSERT(((uint32_t*)thread->t_stack)[3] == THREAD_STACK_MAGIC);
+//		KASSERT(((uint32_t*)thread->t_stack)[0] == THREAD_STACK_MAGIC);
+//		KASSERT(((uint32_t*)thread->t_stack)[1] == THREAD_STACK_MAGIC);
+//		KASSERT(((uint32_t*)thread->t_stack)[2] == THREAD_STACK_MAGIC);
+//		KASSERT(((uint32_t*)thread->t_stack)[3] == THREAD_STACK_MAGIC);
 	}
 }
 
